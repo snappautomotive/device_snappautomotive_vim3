b23883871adc5f7898a82e56672811ce3c913f9e
Al Sutton <al.sutton@alsutton.com>
2024-11-08 07:35:47 +0000

    Initial import
    Change-Id: I59eb55561c63976d2c8069a4fd330b3cbf61244a

---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+# Copyright (c)2024 Snapp Automotive Ltd.
+
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/snapp_car_vim3.mk 
+
+COMMON_LUNCH_CHOICES := snapp_car_vim3-eng
---
+++ snapp_car_vim3.mk
@@ -0,0 +1,81 @@
+TARGET_VIM3 := true
+TARGET_USE_TABLET_LAUNCHER := true
+
+BOARD_IS_AUTOMOTIVE := true
+PRODUCT_DISPLAY_DENSITY := 160
+
+$(call inherit-product, device/snappautomotive/common/additions.mk)
+$(call inherit-product, device/amlogic/yukawa/yukawa.mk)
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
+
+PRODUCT_PACKAGES += \
+	android.hardware.automotive.vehicle@2.0-default-service \
+	android.hardware.broadcastradio@2.0-service \
+	android.hardware.automotive.remoteaccess@V2-default-service \
+	android.hardware.automotive.ivn@V1-default-service \
+	CarConnectivityOverlay \
+	librs_jni
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml \
+	frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
+	frameworks/native/data/etc/android.hardware.type.automotive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.type.automotive.xml \
+	frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml \
+	frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+	frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.ar.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.any.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	android.car.drawer.unlimited=true \
+	android.car.hvac.demo=true \
+	com.android.car.radio.demo=true \
+	com.android.car.radio.demo.dual=true \
+	ro.hardware.egl=mali
+
+PRODUCT_COPY_FILES += \
+	packages/services/Car/car_product/init/init.bootstat.rc:root/init.bootstat.rc \
+	packages/services/Car/car_product/init/init.car.rc:root/init.car.rc
+
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
+	device/snappautomotive/vim3/automotive_framework_compatibility_matrix.xml
+
+# EVS - Use the mocked EVS from the emulator builds. This can be replaced
+# with a full EVS HAL implementation to integrate with the VIM3 hardware
+# at a later date.
+ENABLE_EVS_SAMPLE ?= false
+ENABLE_EVS_SERVICE ?= true
+ENABLE_MOCK_EVSHAL ?= true
+ENABLE_CAREVSSERVICE_SAMPLE ?= false
+ENABLE_SAMPLE_EVS_APP ?= false
+ENABLE_CARTELEMETRY_SERVICE ?= false
+ifeq ($(ENABLE_MOCK_EVSHAL), true)
+CUSTOMIZE_EVS_SERVICE_PARAMETER := true
+endif  # ENABLE_MOCK_EVSHAL
+$(call inherit-product, device/generic/car/emulator/evs/evs.mk)
+
+# CAN bus support - We don't have a direct hardware interface on the
+# VIM3, but there are dongles which can provide CAN bus connectivity
+
+PRODUCT_PACKAGES += \
+	android.hardware.automotive.can@1.0-service
+PRODUCT_PACKAGES_DEBUG += \
+	canhalctrl \
+	canhaldump \
+	canhalsend
+
+PRODUCT_NAME := snapp_car_vim3
+PRODUCT_MODEL := SnappOS for VIM3
