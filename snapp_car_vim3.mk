9ae78e2d67d0cfbebb9bbea194030fc28cbf7420
Al Sutton <al.sutton@alsutton.com>
2024-11-27 15:32:21 +0000

    Modifications to move from A13 to A12.1

--- snapp_car_vim3.mk
+++ snapp_car_vim3.mk
@@ -1,5 +1,6 @@
 TARGET_VIM3 := true
 TARGET_USE_TABLET_LAUNCHER := true
+TARGET_USES_CAR_FUTURE_FEATURES := true
 
 BOARD_IS_AUTOMOTIVE := true
 PRODUCT_DISPLAY_DENSITY := 160
@@ -7,7 +8,7 @@ PRODUCT_DISPLAY_DENSITY := 160
 # EVS - Use the mocked EVS from the emulator builds. This can be replaced
 # with a full EVS HAL implementation to integrate with the VIM3 hardware
 # at a later date.
-ENABLE_EVS_SAMPLE := false
+ENABLE_EVS_SAMPLE := true
 ENABLE_EVS_SERVICE := true
 ENABLE_MOCK_EVSHAL := true
 ENABLE_SAMPLE_EVS_APP := false
@@ -25,13 +26,11 @@ $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 $(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 
 PRODUCT_PACKAGES += \
-	android.hardware.automotive.vehicle@2.0-default-service \
-	android.hardware.automotive.vehicle@V3-default-service \
+	android.hardware.automotive.audiocontrol-service.example \
+	android.hardware.automotive.vehicle@2.0-service \
+	android.hardware.automotive.evs.manager@1.1 \
+	android.hardware.automotive.evs@1.1-sample \
 	android.hardware.broadcastradio@2.0-service \
-	android.hardware.broadcastradio \
-	android.automotive.evs.manager@1.1 \
-	android.hardware.automotive.remoteaccess@V2-default-service \
-	android.hardware.automotive.ivn@V1-default-service \
 	CarConnectivityOverlay \
 	CarWifiOverlay \
 	librs_jni
@@ -42,6 +41,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
 	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
 	frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.hardware.type.automotive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.type.automotive.xml \
@@ -62,12 +62,16 @@ PRODUCT_COPY_FILES += \
 	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
 	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
 	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+	device/google_car/common/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	android.car.drawer.unlimited=true \
 	android.car.hvac.demo=true \
 	com.android.car.radio.demo=true \
 	com.android.car.radio.demo.dual=true \
+	ro.boot.wificountrycode=00 \
+	ro.carwatchdog.vhal_healthcheck.interval=10 \
+	ro.config.media_vol_default=0 \
 	ro.hardware.egl=mali
 
 PRODUCT_COPY_FILES += \
@@ -96,6 +100,11 @@ PRODUCT_PACKAGES_DEBUG += \
 # Sepolicy for occupant awareness system
 include packages/services/Car/car_product/occupant_awareness/OccupantAwareness.mk
 
+# Copy APN configs
+PRODUCT_COPY_FILES += \
+    device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml
+
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.boot.wificountrycode=00 \
         ro.config.media_vol_default=0 \
@@ -103,3 +112,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_NAME := snapp_car_vim3
 PRODUCT_MODEL := SnappOS for VIM3
+PRODUCT_BRAND := Android
